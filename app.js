da5cc16584e7255e2eeface13e6dac688796dcd5
Utkarsh Vishnoi <utkarshvishnoi25@gmail.com>
2017-10-31 20:59:29 +0530

    Added support for smart PI

--- app.js
+++ app.js
@@ -1,3 +1,57 @@
 var io = require('socket.io')(80);
 
 console.log("Socket server running on port 80");
+
+var identifier = "#5521SHCBUV";
+var pi_ID, lights, temperature, humidity;
+
+io.on('connection', function(client){
+  client.auth = false;
+  client.on('authenticate', function(data){
+    var recData = JSON.parse(data);
+    if(identifier == recData.identifier) {
+      client.auth = true;
+    }
+    switch (recData.type) {
+      case "smartPi":
+        pi_ID = client.id;
+        console.log("----------------------------------------");
+        console.log("Smart PI connected.");
+        console.log("Authentication ID - " + client.id);
+        console.log("----------------------------------------");
+        client.on('status', function(data){
+          var status = JSON.parse(data);
+          lights = status.lights;
+          temperature = status.temperature;
+          humidity = status.humidity;
+          // console.log(lights[1]);
+          // console.log(lights[2]);
+          // console.log(lights[3]);
+
+        });
+        client.emit("authenticated");
+        break;
+      case "smartUsr":
+        if(pi_ID != null) {
+          console.log("---------------------------------------");
+          console.log("Smart User connected.");
+          console.log("Authentication ID - " + client.id);
+          console.log("---------------------------------------");
+          client.emit("authenticated");
+        }
+        break;
+    }
+  });
+
+  setTimeout(function() {
+    if (!client.auth) {
+      console.log("Disconnecting socket ", client.id);
+      client.disconnect('unauthorized');
+    }
+  }, 1000);
+
+});
+
+io.on("disconnect", function() {
+  pi_ID = null;
+});
