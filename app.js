70cce489df62e0cfe08531008074ea56f4be222c
Utkarsh Vishnoi <utkarshvishnoi25@gmail.com>
2018-06-03 19:37:03 +0530

    v 1.0 Release

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var port = (process.env.PORT || 80);
+var port = (process.env.PORT || 8090);
 var io = require('socket.io').listen(port);
 var requests = require('axios');
+var moment = require('moment');
 var ip = require('ip');
 
 console.log("Socket server started!");
@@ -9,9 +10,10 @@ console.log(ip.address() + ":" + port);
 var identifier = "#5521SHCBUV";
 var pi_ID, user_ID;
 var pi_client, user_client = null;
-var temperature, humidity, lights = {1: false, 2: false, 3: false};
 var packet = {};
+packet.lights = {1: false, 2: false, 3: false};
 
+var config = {	headers: {'Content-Type': 'application/json','Cache-Control' : 'no-cache'}};
 
 var pi_namespace = io.of("/smart-pi");
 var user_namespace = io.of("/smart-user");
@@ -66,26 +68,30 @@ pi_namespace.on("connection", function (client) {
         console.log("Authentication ID - " + client.id);
         console.log("----------------------------------------");
 
-        client.on('dht', function(data){
-            var pData = JSON.parse(data);
-            temperature = pData.temperature;
-            humidity = pData.humidity;
-            requests.get('http://139.59.67.201/update', {
-                params: {
-                    api_key: "J95Q2XY2YKFL2OLP",
-                    field1: temperature,
-                    field2: humidity
-                }
-            });
+        client.on('lights', function (data) {
+            packet.lights = JSON.parse(data);
+            if(user_client != null)
+                user_client.emit('switch_backflip', packet.lights);
         });
 
-        client.on('lights', function (data) {
-            lights = JSON.parse(data);
-            if(user_client != null)
-                user_client.emit('switch_backflip', lights);
+        client.on("dhtResponse", function (data) {
+            var tSLASHh = data.split("|");
+            requests.get('http://139.59.83.220/update', {
+                params: {
+                    api_key: "WKHLKHVJPX8ZC6QM",
+                    field1: tSLASHh[0],
+                    field2: tSLASHh[1]
+                }
+            }, config);
         });
 
         client.emit("authenticated");
+        dht();
+        setInterval(dht, 60000);
+
+        function dht() {
+            client.emit("dhtRequest");
+        }
     };
 });
 
@@ -115,10 +121,63 @@ user_namespace.on("connection", function (client) {
         console.log("---------------------------------------");
 
         client.on("requestStatus", function () {
-            packet.temperature = temperature;
-            packet.humidity = humidity;
-            packet.lights = lights;
-            client.emit("statusResponse", packet);
+            requests.get('http://139.59.83.220/channels/1/feed.json', {
+                params:{
+                    key : "D3YB1P5MKS27LHRO",
+                    results: 1440,
+                    days: 1,
+                    average: 60,
+                    offset: 5
+                }
+            }, config).then(function(response) {
+                var data = response.data;
+                var feeds = data['feeds'];
+                var lastUpdated = data['channel'].updated_at;
+                var lUpdated = lastUpdated.split("T");
+                var ludate = lUpdated[0];
+                var lutime = lUpdated[1].split("+")[0];
+                var ludnt = moment(ludate + " " + lutime).add(30, "m");
+                var flu = ludnt.format("DD-MM HH:mm");
+                var temperatures = [];
+                var humidities = [];
+                var unixtimestamps = [];
+                var timestamps = [];
+                for(var i=0;i<feeds.length;i++) {
+                    var feed = feeds[i];
+                    if(feed['field1']) {
+                        temperatures[temperatures.length] = feed['field1'];
+                        humidities[humidities.length] = feed['field2'];
+                        var created_at = feed['created_at'];
+                        var timestamp = created_at.split("T");
+                        var date = timestamp[0];
+                        var time = timestamp[1].split("+")[0];
+                        var dnt = moment(date + " " + time).add(30, "m");
+                        timestamps[timestamps.length] = dnt.unix();
+                        unixtimestamps[unixtimestamps.length] = dnt.unix();
+                    }
+                }
+                var baseStamp;
+                for(var j = 0; j < unixtimestamps.length;j++) {
+                    if(j === 0) {
+                        baseStamp=unixtimestamps[j];
+                        unixtimestamps[j] = 0;
+                    }
+                    else {
+                        unixtimestamps[j] = unixtimestamps[j] - baseStamp;
+                    }
+                }
+                var graph = {};
+                var timePacket = {};
+                timePacket.timeDifference = unixtimestamps;
+                timePacket.unix = timestamps;
+                timePacket.lastUpdated = flu;
+                graph.temperature = temperatures;
+                graph.humidity = humidities;
+                graph.time = timePacket;
+                packet.graph = graph;
+                client.emit("statusResponse", packet);
+                console.log(packet);
+            });
         });
 
         client.on("switch", function (light, state) {
--- test.js
+++ test.js
@@ -1,137 +1,50 @@
-var port = (process.env.PORT || 80);
-var io = require('socket.io').listen(port);
 var requests = require('axios');
-var ip = require('ip');
+var moment = require('moment');
 
-console.log("Socket server started!");
-console.log(ip.address() + ":" + port);
-
-var identifier = "#5521SHCBUV";
-var pi_ID, user_ID;
-var pi_client;
-var temperature = 32, humidity = 45, lights = {1: false, 2: true, 3: true};
-var packet = {};
-
-
-var pi_namespace = io.of("/smart-pi");
-var user_namespace = io.of("/smart-user");
-
-var disconnect = function(id) {
-    if (id === pi_ID) {
-        pi_ID = null;
-        console.log("----------------------------------------");
-        console.log("Smart PI disconnected.");
-        console.log("ID - " + id);
-        console.log("----------------------------------------");
+requests.get('http://127.0.0.1/feed.json').then(function(response) {
+    var data = response.data;
+    var feeds = data['feeds'];
+    var lastUpdated = data['channel'].updated_at;
+    var lUpdated = lastUpdated.split("T");
+    var ludate = lUpdated[0];
+    var lutime = lUpdated[1].split("+")[0];
+    var ludnt = moment(ludate + " " + lutime).add(30, "m");
+    var flu = ludnt.format("MM-DD HH:mm");
+    var temperatures = [];
+    var humidities = [];
+    var unixtimestamps = [];
+    var timestamps = [];
+    for(var i=0;i<feeds.length;i++) {
+        var feed = feeds[i];
+        if(feed['field1']) {
+            temperatures[temperatures.length] = feed['field1'];
+            humidities[humidities.length] = feed['field2'];
+            var created_at = feed['created_at'];
+            var timestamp = created_at.split("T");
+            var date = timestamp[0];
+            var time = timestamp[1].split("+")[0];
+            var dnt = moment(date + " " + time).add(30, "m");
+            timestamps[timestamps.length] = dnt.format("MM-DD HH:mm");
+            unixtimestamps[unixtimestamps.length] = dnt.unix();
+        }
     }
-    else {
-        user_ID = null;
-        console.log("----------------------------------------");
-        console.log("Smart User disconnected.");
-        console.log("ID - " + id);
-        console.log("----------------------------------------");
+    var baseStamp;
+    for(var j = 0; j < unixtimestamps.length;j++) {
+        if(j === 0) {
+            baseStamp=unixtimestamps[j];
+            unixtimestamps[j] = 0;
+        }
+        else {
+            unixtimestamps[j] = unixtimestamps[j] - baseStamp;
+        }
     }
-};
-var timeOut = function(client) {
-    setTimeout(function() {
-        if (!client.auth) {
-            console.log("Disconnecting socket ", client.id);
-            client.disconnect('unauthorized');
-        }
-    }, 1000);
-};
-
-pi_namespace.on("connection", function (client) {
-    client.auth = false;
-    pi_client = client;
-
-    client.on("authenticate", function (data) {
-        var authData = JSON.parse(data);
-        if(identifier === authData.identifier) {
-            client.auth = true;
-            authenticated();
-        }
-    });
-
-    timeOut(client);
-
-    client.on("disconnect", function() {
-        disconnect(client.id)
-    });
-
-    var authenticated = function() {
-        pi_ID = client.id;
-        console.log("----------------------------------------");
-        console.log("Smart PI connected.");
-        console.log("Authentication ID - " + client.id);
-        console.log("----------------------------------------");
-
-        client.on('dht', function(data){
-            var pData = JSON.parse(data);
-            temperature = pData.temperature;
-            humidity = pData.humidity;
-            // console.log(data); http://192.168.43.177/update?api_key=KSR6IBTU5N51WE10&field3=1
-            requests.get('http://192.168.43.177/update', {
-                params: {
-                    api_key: "CMJ37A74GVAWNQ9X",
-                    field1: temperature,
-                    field2: humidity
-                }
-            });
-        });
-
-        client.on('lights', function (data) {
-            lights = JSON.parse(data);
-            console.log(lights);
-        });
-
-        client.emit("authenticated");
-    };
-});
-
-user_namespace.on("connection", function (client) {
-    client.auth = false;
-
-    client.on("authenticate", function (data) {
-        var authData = JSON.parse(data);
-        if(identifier === authData.identifier) {
-            client.auth = true;
-            authenticated();
-        }
-    });
-
-    timeOut(client);
-
-    client.on("disconnect", function() {
-        disconnect(client.id)
-    });
-
-    var authenticated = function () {
-        user_ID = client.id;
-        console.log("---------------------------------------");
-        console.log("Smart User connected.");
-        console.log("Authentication ID - " + client.id);
-        console.log("---------------------------------------");
-
-        client.on("requestStatus", function () {
-            packet.temperature = temperature;
-            packet.humidity = humidity;
-            packet.lights = lights;
-            client.emit("statusResponse", packet);
-        });
-
-        client.on("switch", function (light, state) {
-            var boolState;
-            if(state) {
-                boolState = 1;
-            }
-            else {
-                boolState = 0;
-            }
-            console.log("Toggle Activated: " + light + " || " + state);
-            pi_client.emit('toggle', light, boolState);
-        });
-
-        client.emit("authenticated");
-    };
+    var graph = {};
+    var timePacket = {};
+    timePacket.timestamps = unixtimestamps;
+    timePacket.label = timestamps;
+    timePacket.lastUpdated = flu;
+    graph.temperature = temperatures;
+    graph.humidity = humidities;
+    graph.time = timePacket;
+    console.log(graph);
 });
